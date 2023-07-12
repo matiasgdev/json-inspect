ab3c312e40a838a33661c8f52ce3214ae5f2faf9
Matias Navarro <matiasg.developer@gmail.com>
2023-07-12 20:22:55 -0300

    feat: added basic ui and input/output

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,46 @@
-import {useState} from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {ChangeEventHandler, useState} from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [json, setJSON] = useState('')
+
+  const handleChange: ChangeEventHandler<HTMLInputElement> = event => {
+    const file = event.target.files?.[0] as File
+    if (!file) return
+
+    const reader = new FileReader()
+    reader.readAsText(file, 'utf-8')
+    reader.onloadend = event => {
+      setJSON(JSON.stringify(event.target?.result, null, 4))
+    }
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main className="min-h-screen bg-gray-100">
+      <header className="min-h-[75px] min-w-screen bg-purple-700 flex items-center px-4">
+        <h1 className="text-2xl text-white uppercase text-bold">
+          json <span className="text-yellow-200">inspector</span>
+        </h1>
+      </header>
+      <section className="h-[calc(100vh-75px)] w-full flex justify-center items-center">
+        <div className="flex w-full h-full p-4">
+          <button className="max-h-[40px] bg-purple-300 py-1 px-2 hover:bg-purple-400 transition-colors cursor-pointer border-none outline-none">
+            <label htmlFor="file" className="text-white cursor-pointer">
+              <p>Add here</p>
+            </label>
+            <input
+              type="file"
+              id="file"
+              accept="application/json"
+              className="hidden"
+              onChange={handleChange}
+            />
+          </button>
+        </div>
+        <div className="flex w-full h-full p-4 border border-red-500 border-solid grow-0">
+          {json}
+        </div>
+      </section>
+    </main>
   )
 }
 
