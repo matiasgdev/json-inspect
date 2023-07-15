b74d90aaad424aae4cc47633a0d082f255d4b092
Matias Navarro <matiasg.developer@gmail.com>
2023-07-14 21:56:36 -0300

    feat: add lines counter

--- src/components/DisplayJSON/index.tsx
+++ src/components/DisplayJSON/index.tsx
@@ -1,3 +1,4 @@
+import {useMemo} from 'react'
 import {Reader} from '../../utils/formatJSON'
 
 interface DisplayJSONProps {
@@ -5,6 +6,10 @@ interface DisplayJSONProps {
 }
 
 export const DisplayJSON: React.FC<DisplayJSONProps> = ({readableJSON}) => {
+  const lines = useMemo(() => {
+    return Array.from({length: readableJSON?.length}, (_, k) => k + 1)
+  }, [readableJSON])
+
   const render = (value: Reader[]) =>
     value.map(({key, value}) => (
       <details key={key}>
@@ -25,5 +30,14 @@ export const DisplayJSON: React.FC<DisplayJSONProps> = ({readableJSON}) => {
 
   if (!readableJSON) return
 
-  return <div className="w-full">{render(readableJSON)}</div>
+  return (
+    <div className="relative w-full">
+      <div className="absolute w-[40px] left-0 top-0 h-full">
+        {lines.map(lineNumber => {
+          return <div key={lineNumber}>{lineNumber}</div>
+        })}
+      </div>
+      <div className="ml-6">{render(readableJSON)}</div>
+    </div>
+  )
 }
