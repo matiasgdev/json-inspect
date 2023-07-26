a5e421644a6963d6be3d75930cfec4d720fb620b
Matias Navarro <matiasg.developer@gmail.com>
2023-07-26 10:37:05 -0300

    enhance: added more metadata to json nodes props

--- src/hooks/useJsonProperties.ts
+++ src/hooks/useJsonProperties.ts
@@ -1,38 +1,52 @@
 import {useMemo} from 'react'
 import {useJsonStore} from '../stores/json-store'
-import {flatten} from 'flat'
+import {TypeOf, type} from '../utils/type'
 
 export interface JsonProperties {
   key: string
-  deeperKey: string
-  value: JsonProperties[] | string | number | boolean | Date
+  value: JsonProperties[] | TypeOf
+  typeofValue: TypeOf
+  parentKey?: string
+  nodeIndex: number
 }
 
-function buildJsonProperties(
+function flatJsonProperties(
   json: object | null,
+  cacheProps: JsonProperties[] = [],
   parentKey = '',
-): {props: JsonProperties[]; flattenObj: object} | null {
+): JsonProperties[] | null {
   if (json === null) return json
 
-  const props: JsonProperties[] = []
+  let properties: JsonProperties[] = [...cacheProps]
 
   for (const key of Object.keys(json)) {
     const value = json[key as keyof typeof json]
     const deeperKey = parentKey ? `${parentKey}.${key}` : key
+    const typeofValue = type(value)
+    const newValue: JsonProperties = {
+      key: deeperKey,
+      value,
+      typeofValue,
+      nodeIndex: properties.length,
+    }
 
-    props.push({
-      key: key,
-      deeperKey,
-      value:
-        typeof value === 'object'
-          ? buildJsonProperties(value, deeperKey)!.props
-          : value,
-    })
+    if (parentKey) {
+      newValue.parentKey = parentKey
+    }
+
+    properties.push(newValue)
+
+    const shouldFlat = typeofValue === 'array' || typeofValue === 'object'
+
+    if (shouldFlat) {
+      properties = flatJsonProperties(value, properties, deeperKey)!
+    }
   }
-  return {props, flattenObj: flatten(json)}
+
+  return properties
 }
 
 export function useJsonProperties() {
   const json = useJsonStore(s => s.json)
-  return useMemo(() => buildJsonProperties(json), [json])
+  return useMemo(() => flatJsonProperties(json), [json])
 }
