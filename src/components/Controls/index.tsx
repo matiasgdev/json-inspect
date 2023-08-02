a5c146eb78786e6c88f84a438caff37231d99a52
Matias Navarro <matiasg.developer@gmail.com>
2023-08-02 10:27:31 -0300

    feat: organize node paths indicator with arrow

--- src/components/Controls/index.tsx
+++ src/components/Controls/index.tsx
@@ -1,22 +1,35 @@
 import {useMemo} from 'react'
 import {ReactComponent as SearchIcon} from '../../assets/icons/search.svg'
 import {useJsonStore} from '../../stores/json-store'
+import {ReactComponent as ArrowRight} from '../../assets/icons/nav-arrow-right.svg'
 
 export const Controls = () => {
   const {selectedIndex} = useJsonStore(s => ({
     selectedIndex: s.selectedNodeIndex,
   }))
-  const nodeIndex = useMemo(() => {
-    if (!selectedIndex) {
-      return 'root'
-    }
-    return `root.`.concat(selectedIndex)
+  const nodePaths = useMemo(() => {
+    const paths = ['root']
+    selectedIndex.split('.').forEach(index => {
+      paths.push(index)
+    })
+    return paths
   }, [selectedIndex])
+
   return (
     <div className="flex items-center justify-between border-b-slate-600  border-b-[1px]  p-2 pl-4 mb-4 sticky">
-      <span className="text-white/70 text-sm bg-slate-800 rounded-sm px-2">
-        {nodeIndex}
-      </span>
+      <div className="flex items-center text-white/70 text-sm bg-slate-800 rounded-sm px-2">
+        {nodePaths.map((path, index) => {
+          const isLast = index + 1 === nodePaths.length
+          return (
+            <div className="flex items-center justify-start gap-x-[4px] mr-[4px]">
+              <div className="cursor-pointer hover:text-white">{path}</div>
+              {isLast ? null : (
+                <ArrowRight className="h-[11.5px] w-[11.5px] block" />
+              )}
+            </div>
+          )
+        })}
+      </div>
       <div className="flex items-center max-w-[180px] px-2 rounded-[.2rem] bg-slate-800">
         <input
           placeholder="Search..."
